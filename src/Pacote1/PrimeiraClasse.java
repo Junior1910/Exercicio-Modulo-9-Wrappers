e01276228f5e907b751dc2156ef99a4bc96f3230
Florisvaldo <juniortcj10012@hotmail.com>
2024-02-01 17:39:52 -0300

    Variavel idade convertida para tipo Rapper

---
+++ src/Pacote1/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package Pacote1;
+
+public class PrimeiraClasse {
+    public static void main(String args[]){
+        int idade = 38;
+        System.out.println(idade);
+        Integer idade2 = 38;
+        System.out.println(idade2);
+        }
+}

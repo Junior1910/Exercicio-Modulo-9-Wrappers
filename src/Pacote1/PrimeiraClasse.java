dd051244d0cea528b5b4c2b15f4e1dbac7eecb5e
Florisvaldo <juniortcj10012@hotmail.com>
2024-02-01 19:59:01 -0300

    Linha 7 corrigida

--- src/Pacote1/PrimeiraClasse.java
+++ src/Pacote1/PrimeiraClasse.java
@@ -3,8 +3,10 @@ package Pacote1;
 public class PrimeiraClasse {
     public static void main(String args[]){
         int idade = 38;
+
+        Integer idadeInteger = Integer.valueOf(idade);
+
         System.out.println(idade);
-        Integer idade2 = 38;
-        System.out.println(idade2);
-        }
+        System.out.println(idade);
+    }
 }
